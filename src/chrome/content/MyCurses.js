8a105f772872679561c71a7e8b06627d2f17036e
Rasmus Eskola <fruitiex@gmail.com>
2015-10-27 18:37:50 +0200

    MyCurses for mozilla

--- src/chrome/content/MyCurses.js
+++ src/chrome/content/MyCurses.js
@@ -1,46 +1,39 @@
 (function() {
-
-    function walk(node) 
-    {
+    function walk(node) {
         // I stole this function from here:
         // http://is.gd/mwZp7E
-    
+
         var child, next;
-    
-        switch ( node.nodeType )  
+
+        switch ( node.nodeType )
         {
             case 1:  // Element
             case 9:  // Document
             case 11: // Document fragment
                 child = node.firstChild;
-                while ( child ) 
+                while ( child )
                 {
                     next = child.nextSibling;
                     walk(child);
                     child = next;
                 }
                 break;
-    
+
             case 3: // Text node
                 handleText(node);
                 break;
         }
     }
-    
-    function handleText(textNode)
-    {
+
+    function handleText(textNode) {
         var v = textNode.nodeValue;
-    
-        v = v.replace(/\bThe Cloud\b/g, "My Butt");
-        v = v.replace(/\bThe cloud\b/g, "My butt");
-        v = v.replace(/\bthe Cloud\b/g, "my Butt");
-        v = v.replace(/\bthe cloud\b/g, "my butt");
-    
+
+        v = v.replace(/\bMyCourses/g, 'MyCurses');
+        v = v.replace(/\bMy own courses/g, 'My own curses');
         textNode.nodeValue = v;
     }
 
-    function windowLoadHandler()
-    {
+    function windowLoadHandler() {
         // Dear Mozilla: I hate you for making me do this.
         window.removeEventListener('load', windowLoadHandler);
 
